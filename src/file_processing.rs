9bdf9e671ccfe088c0229f037029c48c3c5eaa5d
Marko Rupnik <marko.markorupnik@gmail.com>
2023-08-03 14:19:14 +0200

    add scaffolding for processing directories

---
+++ src/file_processing.rs
@@ -0,0 +1,28 @@
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::io::Result as IoResult;
+use walkdir::WalkDir;
+use rayon::prelude::*;
+
+pub fn process_directory(input_dir: &str) -> IoResult<()> {
+    let input_path = Path::new(input_dir);
+    let output_dir = format!("{}_output", input_path.display());
+    fs::create_dir_all(&output_dir)?;
+
+    let files: Vec<PathBuf> = WalkDir::new(input_path)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.file_type().is_file())
+        .map(|entry| entry.path().to_owned())
+        .collect();
+
+    files.par_iter().for_each(|file| {
+        let file_name = file.file_stem().and_then(|f| f.to_str()).unwrap_or("output");
+        let output_file_path = Path::new(&output_dir).join(file_name);
+        if let Err(err) = crate::compress_to_file(file.to_str().unwrap(), output_file_path.to_str().unwrap()) {
+            eprintln!("Error processing file: {}: {}", file.display(), err);
+        }
+    });
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,34 @@
 use std::env;
-
 use compact_sequence::{
     compress_to_file,
     unpack_from_file,
-}; 
+};
+mod file_processing;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    
+
     if args.len() < 2 {
-        println!("Usage: cargo run <input_file_name> <output_file_name> [-u]");
+        println!("Usage: cargo run <input_file_or_directory> [-u]");
         return Ok(());
     }
 
-    let input_file_name = &args[1];
-    let output_file_name = &args[2];
+    let input_path = &args[1];
 
-    if args.len() >= 4 && args[3] == "-u" {
-        unpack_from_file(input_file_name, output_file_name)?;
+    if input_path.ends_with(".txt") {
+        let output_path = format!("{}_output.txt", input_path);
+        if args.len() >= 3 && args[2] == "-u" {
+            unpack_from_file(input_path, &output_path)?;
+        } else {
+            compress_to_file(input_path, &output_path)?;
+        }
+    } else if input_path.ends_with(".fasta") {
+        // Add support for fasta processing here if required
+    } else if input_path.ends_with('/') || input_path.ends_with('\\') {
+        file_processing::process_directory(input_path)?;
     } else {
-        compress_to_file(input_file_name, output_file_name)?;
+        println!("Error: Unsupported file format or invalid path.");
+        return Err("Unsupported file format or invalid path".into());
     }
 
     println!("File processing completed!");
