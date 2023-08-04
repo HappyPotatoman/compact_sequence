3b7ab427ee0d25727710361b5e82001b22f5ad4b
Marko Rupnik <marko.markorupnik@gmail.com>
2023-08-04 13:59:26 +0200

    fix indentation of tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -31,52 +31,52 @@ mod tests {
     }
 
     #[test]
-fn test_compress_to_file() {
-    let input_strings = vec!["AAAA", "AC", "AAAACCCGTT", "AGGGGCCCCTTTTAA",""];
-    let input_file_name = "test_input_compress.txt";
-    let mut input_file = File::create(input_file_name).unwrap();
-    for s in &input_strings {
-        writeln!(input_file, "{}", s).unwrap();
+    fn test_compress_to_file() {
+        let input_strings = vec!["AAAA", "AC", "AAAACCCGTT", "AGGGGCCCCTTTTAA",""];
+        let input_file_name = "test_input_compress.txt";
+        let mut input_file = File::create(input_file_name).unwrap();
+        for s in &input_strings {
+            writeln!(input_file, "{}", s).unwrap();
+        }
+
+        let output_file_name = "test_output_compress.txt";
+        compress_to_file(input_file_name, output_file_name).unwrap();
+
+        let output_file = File::open(output_file_name).unwrap();
+        let reader = BufReader::new(output_file);
+        let output_lines: Vec<_> = reader.lines().map(|line| line.unwrap()).collect();
+
+        for (input, output) in input_strings.iter().zip(output_lines.iter()) {
+            let expected_len = (input.len() + 2) / 3;
+            assert_eq!(output.len(), expected_len);
+        }
+
+        std::fs::remove_file(input_file_name).unwrap();
+        std::fs::remove_file(output_file_name).unwrap();
     }
 
-    let output_file_name = "test_output_compress.txt";
-    compress_to_file(input_file_name, output_file_name).unwrap();
+    #[test]
+    fn test_unpack_from_file() {
+        let input_strings = vec!["A4", "AC", "A4C3G1T2", "AG4C4T4A2"];
+        let input_file_name = "test_input_unpack.txt";
+        let mut input_file = File::create(input_file_name).unwrap();
+        for s in &input_strings {
+            writeln!(input_file, "{}", s).unwrap();
+        }
 
-    let output_file = File::open(output_file_name).unwrap();
-    let reader = BufReader::new(output_file);
-    let output_lines: Vec<_> = reader.lines().map(|line| line.unwrap()).collect();
+        let output_file_name = "test_output_unpack.txt";
+        unpack_from_file(input_file_name, output_file_name).unwrap();
 
-    for (input, output) in input_strings.iter().zip(output_lines.iter()) {
-        let expected_len = (input.len() + 2) / 3;
-        assert_eq!(output.len(), expected_len);
+        let output_file = File::open(output_file_name).unwrap();
+        let reader = BufReader::new(output_file);
+        let output_lines: Vec<_> = reader.lines().map(|line| line.unwrap()).collect();
+
+        for (input, output) in input_strings.iter().zip(output_lines.iter()) {
+            assert!(output.len() >= input.len());
+        }
+
+        std::fs::remove_file(input_file_name).unwrap();
+        std::fs::remove_file(output_file_name).unwrap();
     }
-
-    std::fs::remove_file(input_file_name).unwrap();
-    std::fs::remove_file(output_file_name).unwrap();
-}
-
-#[test]
-fn test_unpack_from_file() {
-    let input_strings = vec!["A4", "AC", "A4C3G1T2", "AG4C4T4A2"];
-    let input_file_name = "test_input_unpack.txt";
-    let mut input_file = File::create(input_file_name).unwrap();
-    for s in &input_strings {
-        writeln!(input_file, "{}", s).unwrap();
-    }
-
-    let output_file_name = "test_output_unpack.txt";
-    unpack_from_file(input_file_name, output_file_name).unwrap();
-
-    let output_file = File::open(output_file_name).unwrap();
-    let reader = BufReader::new(output_file);
-    let output_lines: Vec<_> = reader.lines().map(|line| line.unwrap()).collect();
-
-    for (input, output) in input_strings.iter().zip(output_lines.iter()) {
-        assert!(output.len() >= input.len());
-    }
-
-    std::fs::remove_file(input_file_name).unwrap();
-    std::fs::remove_file(output_file_name).unwrap();
-}
 }
 
